61bf6af0a5e23d8304b08a599169b4e79d293e1e
Sarthak-Daga <sarthakdaga21@gmail.com>
2026-06-17 13:24:24 +0530

    Selling works , UI works perfectly

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -3,8 +3,44 @@ import 'package:billing_app/screens/add_record_screen.dart';
 import 'package:billing_app/screens/search_screen.dart';
 import 'package:billing_app/screens/buy_old_device_screen.dart';
 
-class Dashboard extends StatelessWidget {
+import '../database/database_helper.dart';
+
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int totalDevices = 0;
+  int soldDevices = 0;
+  int availableDevices = 0;
+  int oldDevices = 0;
+  int newDevices = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    loadStats();
+  }
+
+  Future<void> loadStats() async {
+    final customers = await DatabaseHelper.instance.getAllCustomers();
+
+    setState(() {
+      totalDevices = customers.length;
+
+      soldDevices = customers.where((c) => c['status'] == 'SOLD').length;
+
+      availableDevices = customers
+          .where((c) => c['status'] == 'AVAILABLE')
+          .length;
+
+      oldDevices = customers.where((c) => c['deviceType'] == 'OLD').length;
+      newDevices = customers.where((c) => c['deviceType'] == 'NEW').length;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,139 +54,240 @@ class Dashboard extends StatelessWidget {
         foregroundColor: Colors.black,
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
 
-            const Text(
-              "Mahadev Telecoms",
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-            ),
+              const Text(
+                "Mahadev Telecoms",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
 
-            const SizedBox(height: 8),
+              const SizedBox(height: 8),
 
-            Text(
-              "Manage customer and device records",
-              style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-            ),
+              Text(
+                "Manage customer and device records",
+                style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+              ),
 
-            const SizedBox(height: 50),
+              const SizedBox(height: 50),
 
-            SizedBox(
-              width: double.infinity,
-              height: 80,
-              child: Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(20),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SearchScreen(),
-                      ),
-                    );
-                  },
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.search, size: 30),
-                      SizedBox(width: 15),
-                      Text(
-                        "Search Records",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
+              SizedBox(
+                width: double.infinity,
+                height: 80,
+                child: Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    onTap: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SearchScreen(),
                         ),
-                      ),
-                    ],
+                      );
+
+                      loadStats();
+                    },
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.search, size: 30),
+                        SizedBox(width: 15),
+                        Text(
+                          "Search Records",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 25),
+              const SizedBox(height: 25),
 
-            SizedBox(
-              width: double.infinity,
-              height: 80,
-              child: Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(20),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const AddRecordScreen(),
-                      ),
-                    );
-                  },
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.add_circle_outline, size: 30),
-                      SizedBox(width: 15),
-                      Text(
-                        "Add New Record",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
+              SizedBox(
+                width: double.infinity,
+                height: 80,
+                child: Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    onTap: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AddRecordScreen(),
                         ),
-                      ),
-                    ],
+                      );
+                      loadStats();
+                    },
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.add_circle_outline, size: 30),
+                        SizedBox(width: 15),
+                        Text(
+                          "Add New Record",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 25),
+              const SizedBox(height: 25),
 
-            SizedBox(
-              width: double.infinity,
-              height: 80,
-              child: Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(20),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const BuyOldDeviceScreen(),
-                      ),
-                    );
-                  },
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.recycling, size: 30),
-                      SizedBox(width: 15),
-                      Text(
-                        "Buy Old Device",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
+              SizedBox(
+                width: double.infinity,
+                height: 80,
+                child: Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(20),
+                    onTap: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const BuyOldDeviceScreen(),
                         ),
-                      ),
-                    ],
+                      );
+                      loadStats();
+                    },
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.recycling, size: 30),
+                        SizedBox(width: 15),
+                        Text(
+                          "Buy Old Device",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 25),
-          ],
+              const SizedBox(height: 25),
+              const Text(
+                "Inventory Overview",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  Expanded(
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          children: [
+                            Text("Total"),
+                            Text(
+                              totalDevices.toString(),
+                              style: const TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+
+                  Expanded(
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          children: [
+                            Text("Sold"),
+                            Text(
+                              soldDevices.toString(),
+                              style: const TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 12),
+              Row(
+                children: [
+                  Expanded(
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          children: [
+                            Text("Available"),
+                            Text(
+                              availableDevices.toString(),
+                              style: const TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+
+                  Expanded(
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          children: [
+                            Text("Old Devices"),
+                            Text(
+                              oldDevices.toString(),
+                              style: const TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
